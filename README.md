7f694a623358b5e8f8a31abe19c98807920ad4ad
Dmytro Serdiuk <github@extsoft.pro>
2024-04-08 13:21:47 -0400

    Add initial version
    The basic support of the AWS IAM Identity Center is provided by
    `scan-ic` and `session-ic` commands.

---
+++ README.md
@@ -0,0 +1,31 @@
+# AWS Credentials
+
+`aws-creds` configures your CLI access to AWS services by setting some
+environment variables in your shell based on
+- IAM Identity Center authentication; or
+- IAM user (access key and secret key) (TODO)
+
+**How does it work?**
+
+You ask `aws-creds` to scan specific connection sources (such as AWS IAM Identity Center URL,
+current local AWS config, etc.), and it generates shell aliases for you (alternatively, you can
+create aliases on your own). Those aliases should be saved to the shell configuration profile
+file like `.bashrc`, `.zshrc`, etc. Once done, the next time you need to authenticate a specific
+connection, you run the appropriate alias. It authenticates depending on the configuration
+and exports the AWS session environment variables to the current shell session. The tools such
+as `aws`, `terraform`, `boto3`, and others will automatically use those variables while executing
+requested commands.
+
+
+## Installation
+TODO
+
+## Getting Started
+
+### AWS IAM Identity Center (AWS SSO)
+
+You should have the IAM Identity Center start URL (like `https://xxxxxx.awsapps.com/start`)
+and its region (like `us-east-1`). `python aws-creds.py scan-ic https://xxxxxx.awsapps.com/start us-east-1` command
+generates all possible login aliases. Pick those you want and save them to the shell configuration profile file.
+Once you run an alias, it will open the browser and ask you to authenticate. After successful authentication,
+it will export the AWS session environment variables to the current shell session.
---
+++ aws-creds.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from dataclasses import dataclass
+from argparse import ArgumentParser, HelpFormatter
+from hashlib import sha1
+import os
+import sys
+
+_me = Path(__file__).absolute()
+_prog = Path(__file__).name.split(".")[0]
+_dependencies_home = _me.parent.joinpath(f".{_prog}")
+
+
+def pip_wtf(dependencies: str) -> None:
+    sys.path = [p for p in sys.path if "-packages" not in p] + [str(_dependencies_home)]
+    os.environ["PATH"] += os.pathsep + str(_dependencies_home.joinpath("bin"))
+    os.environ["PYTHONPATH"] = os.pathsep.join(sys.path)
+    dependencies_hash = _dependencies_home.joinpath(f".d.{sha1(dependencies.encode()).hexdigest()}")
+    if dependencies_hash.exists():
+        return
+    _dependencies_home.mkdir(exist_ok=True)
+    dependencies_hash.touch(exist_ok=True)
+    os.system(" ".join([sys.executable, "-m", "pip", "install", "--target", str(_dependencies_home), dependencies]))
+
+
+if sys.version_info < (3, 7):
+    print("Support Python 3.7 or above", file=sys.stderr)
+    exit(1)
+
+pip_wtf("boto3==1.34.38")
+from botocore.session import Session  # noqa: E402
+
+
+@dataclass
+class IdentityCenter:
+    ic_start_url: str
+    ic_region: str
+
+    def cache_file(self) -> Path:
+        return _dependencies_home.joinpath(f".ic.{sha1(bytes(self.ic_start_url, 'utf-8')).hexdigest()}")
+
+
+def _new_session_token(identity_center: IdentityCenter) -> str:
+    sso_oidc = Session().create_client("sso-oidc", region_name=identity_center.ic_region)
+    client_creds = sso_oidc.register_client(clientName="myapp", clientType="public")
+    device_authorization = sso_oidc.start_device_authorization(
+        clientId=client_creds["clientId"],
+        clientSecret=client_creds["clientSecret"],
+        startUrl=identity_center.ic_start_url,
+    )
+    url = device_authorization["verificationUriComplete"]
+    device_code = device_authorization["deviceCode"]
+    expires_in = device_authorization["expiresIn"]
+    interval = device_authorization["interval"]
+    import webbrowser
+
+    webbrowser.open(url, autoraise=True)
+    code_line = "Authorization code: " + device_authorization["userCode"]
+    print(code_line, end="\r", file=sys.stderr)
+    from time import sleep
+
+    message = code_line
+    for n in range(1, expires_in // interval + 1):
+        sleep(interval)
+        message = code_line + f" (awaiting {n} seconds)"
+        print(message, end="\r", file=sys.stderr)
+        try:
+            return sso_oidc.create_token(
+                grantType="urn:ietf:params:oauth:grant-type:device_code",
+                deviceCode=device_code,
+                clientId=client_creds["clientId"],
+                clientSecret=client_creds["clientSecret"],
+            )["accessToken"]
+        except sso_oidc.exceptions.AuthorizationPendingException:
+            pass
+
+
+def _cached_token(access_token_file_name: Path) -> str:
+    with open(access_token_file_name, "r") as access_token_file:
+        return access_token_file.readline()
+
+
+def _new_token(identity_center: IdentityCenter) -> str:
+    with open(identity_center.cache_file(), "w") as access_token_file:
+        access_token = _new_session_token(identity_center)
+        access_token_file.write(access_token)
+        return access_token
+
+
+def _token(identity_center: IdentityCenter) -> str:
+    while True:
+        if identity_center.cache_file().exists():
+            print("Previous session found...", file=sys.stderr)
+            access_token = _cached_token(identity_center.cache_file())
+        else:
+            print("Initializing new session...", file=sys.stderr)
+            access_token = _new_token(identity_center)
+        try:
+            Session().create_client("sso", region_name=identity_center.ic_region).list_accounts(
+                accessToken=access_token, maxResults=1
+            )
+            return access_token
+        except Exception as error:  # botocore.errorfactory.UnauthorizedException
+            if "UnauthorizedException" not in str(error):
+                raise error
+            identity_center.cache_file().unlink()
+            print("Previous session expired...", file=sys.stderr)
+
+
+def _identity_center_scan(ic: IdentityCenter) -> None:
+    sso = Session().create_client("sso", region_name=ic.ic_region)
+    token = _token(ic)
+    for account in sso.list_accounts(accessToken=token, maxResults=100)["accountList"]:
+        account_name = account["accountName"]
+        account_id = account["accountId"]
+        account_roles = sso.list_account_roles(accessToken=token, accountId=account_id)
+        for role in account_roles["roleList"]:
+            print(
+                "alias "
+                + f"{account_name}-{role['roleName']}".lower().replace(" ", "-").replace(".", "")
+                + f"='eval \"$({_prog} session-ic {ic.ic_start_url} {ic.ic_region} {account_id} {role['roleName']})\"'"
+            )
+
+
+def _connect(ic: IdentityCenter, account_id: str, role: str) -> None:
+    role_creds = (
+        Session()
+        .create_client("sso", region_name=ic.ic_region)
+        .get_role_credentials(roleName=role, accountId=account_id, accessToken=_token(ic))["roleCredentials"]
+    )
+    print(f'export AWS_DEFAULT_REGION="{ic.ic_region}"', file=sys.stdout)
+    print(f'export AWS_ACCESS_KEY_ID="{role_creds["accessKeyId"]}"', file=sys.stdout)
+    print(f'export AWS_SECRET_ACCESS_KEY="{role_creds["secretAccessKey"]}"', file=sys.stdout)
+    print(f'export AWS_SESSION_TOKEN="{role_creds["sessionToken"]}"', file=sys.stdout)
+    print("AWS environment variables are exported!", file=sys.stderr)
+
+
+def main():
+    parser = ArgumentParser(
+        description="Painless CLI authentification using various AWS identities.",
+        prog=_prog,
+        formatter_class=lambda prog: HelpFormatter(prog, width=72),
+    )
+    subparsers = parser.add_subparsers(title="Commands", dest="subcommand")
+    scan_parser = subparsers.add_parser(
+        "scan-ic",
+        description="""
+        The command generates login aliases for each role available in the AWS IAM Identity Center.
+         The aliases should be saved to the to relevant shell configuration file.
+         """,
+        help="generates shell authentication aliases for an AWS Identity Center",
+        formatter_class=lambda prog: HelpFormatter(prog, width=72),
+    )
+    scan_parser.add_argument("ic_start_url", help="AWS IAM Identity Center start URL")
+    scan_parser.add_argument("ic_region", help="AWS IAM Identity Center region")
+
+    session_parser = subparsers.add_parser(
+        "session-ic",
+        description="""
+        The command exports the environment variables suitable for authenticating CLI tools
+         by creating a AWS login sessing based on the AWS Identity Center role.
+         """,
+        help="authenticates an AWS Identity Center role",
+        formatter_class=lambda prog: HelpFormatter(prog, width=72),
+    )
+    session_parser.add_argument("ic_start_url", help="AWS IAM Identity Center start URL")
+    session_parser.add_argument("ic_region", help="AWS IAM Identity Center region")
+    session_parser.add_argument("account_id", help="Account ID")
+    session_parser.add_argument("role_name", help="Role")
+
+    args = parser.parse_args()
+
+    if args.subcommand == "scan-ic":
+        _identity_center_scan(IdentityCenter(args.ic_start_url, args.ic_region))
+    elif args.subcommand == "session-ic":
+        _connect(
+            IdentityCenter(args.ic_start_url, args.ic_region),
+            args.account_id,
+            args.role_name,
+        )
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
